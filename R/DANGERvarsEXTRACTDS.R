6b08523845d115d8205a323d84a839945ef20147
Stuart Wheater <stuart.wheater@arjuna.com>
2020-02-11 19:57:14 +0000

    Minor reformat

--- R/DANGERvarsEXTRACTDS.R
+++ R/DANGERvarsEXTRACTDS.R
@@ -1,62 +1,59 @@
 #'
 #' @export
-#'
+#' 
 
 DANGERvarsEXTRACTDS <- function(xName=NULL,yName=NULL,zName=NULL,uName=NULL,vName=NULL,wName=NULL){
 
- if(!is.null(xName)){
+  if(!is.null(xName)){
     xvar <- eval(parse(text=xName))
-}
-
- if(!is.null(yName)){
+  }
+  if(!is.null(yName)){
     yvar <- eval(parse(text=yName))
-}
-
- if(!is.null(zName)){
+  }
+  if(!is.null(zName)){
     zvar <- eval(parse(text=zName))
-}
+  }
 
- if(!is.null(uName)){
+  if(!is.null(uName)){
     uvar <- eval(parse(text=uName))
-}
- if(!is.null(vName)){
+  }
+  if(!is.null(vName)){
     vvar <- eval(parse(text=vName))
-}
- if(!is.null(wName)){
+  }
+  if(!is.null(wName)){
     wvar <- eval(parse(text=wName))
-}
-	
-varnames.vector<-xName
-output.matrix<-matrix(xvar)
+  }
 
-if(!is.null(yName)){
-varnames.vector<-c(varnames.vector,yName)
-output.matrix<-cbind(output.matrix,yvar)
-}
+  varnames.vector<-xName
+  output.matrix<-matrix(xvar)
 
-if(!is.null(zName)){
-varnames.vector<-c(varnames.vector,zName)
-output.matrix<-cbind(output.matrix,zvar)
-}
+  if(!is.null(yName)){
+    varnames.vector<-c(varnames.vector,yName)
+    output.matrix<-cbind(output.matrix,yvar)
+  }
 
-if(!is.null(uName)){
-varnames.vector<-c(varnames.vector,uName)
-output.matrix<-cbind(output.matrix,uvar)
-}
-if(!is.null(vName)){
-varnames.vector<-c(varnames.vector,vName)
-output.matrix<-cbind(output.matrix,vvar)
-}
-if(!is.null(wName)){
-varnames.vector<-c(varnames.vector,wName)
-output.matrix<-cbind(output.matrix,wvar)
-}
+  if(!is.null(zName)){
+    varnames.vector<-c(varnames.vector,zName)
+    output.matrix<-cbind(output.matrix,zvar)
+  }
 
-DANGERvars<-data.frame(output.matrix)
-names(DANGERvars)<-varnames.vector
-return(DANGERvars)
+  if(!is.null(uName)){
+    varnames.vector<-c(varnames.vector,uName)
+    output.matrix<-cbind(output.matrix,uvar)
+  }
+  if(!is.null(vName)){
+    varnames.vector<-c(varnames.vector,vName)
+    output.matrix<-cbind(output.matrix,vvar)
+  }
+  if(!is.null(wName)){
+    varnames.vector<-c(varnames.vector,wName)
+    output.matrix<-cbind(output.matrix,wvar)
+  }
+
+  DANGERvars<-data.frame(output.matrix)
+  names(DANGERvars)<-varnames.vector
+  return(DANGERvars)
 }
 
 #AGGEGATE FUNCTION
 # DANGERvarsEXTRACTDS
-

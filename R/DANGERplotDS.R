8359e9a47af71fe91d661673d4a7852c62e01e8b
Paul Burton <npb102@newcastle.ac.uk>
2021-06-06 17:14:09 +0100

    Update dsDanger repo Add DANGERdmtEXTRACTDS as serverside function for DANGER function copying a data.frame, matrix or tibble from clientside to serverside Update all headers to minimum required to avoid warning messages

--- R/DANGERplotDS.R
+++ R/DANGERplotDS.R
@@ -1,6 +1,14 @@
-#' 
+#' @title DANGERplotDS
+#' @description DANGERplotDS
+#' @details DANGER function
+#' @param x.name character string naming first object to be
+#' plotted on clientside
+#' from serverside to clientside
+#' @param y.name character string naming second object to be
+#' plotted on clientside
+#' @return DANGER function
+#' @author DataSHIELD Development Team
 #' @export
-#' 
 
 DANGERplotDS <- function (x.name=NULL,y.name=NULL)
 {
